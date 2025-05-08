8c727f739c36d7c7c60d281d8a1fe9d6742832ff
Hasan TURAN <hasanturan@Hasans-MacBook-Pro.local>
2025-05-08 21:29:50 +1000

    improve vector store implementation with AWS S3

--- vector_store.py
+++ vector_store.py
@@ -4,6 +4,7 @@ from langchain_community.vectorstores import FAISS
 import os
 from dotenv import load_dotenv
 import logging
+from langchain_community.document_loaders import S3FileLoader
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -25,23 +26,24 @@ class FaissVectorStore:
     
 
 def create_vector_store(embeddings) -> FAISS:
-    # Determine the absolute path to the data file relative to this script
-    script_dir = os.path.dirname(__file__)
-    data_file_path = os.path.join(script_dir, "data", "test-mobile-mechanics.txt")
-    
-    logger.info(f"Loading documents from: {data_file_path}")
-    loader = TextLoader(data_file_path) # Use absolute path
-    docs = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    all_splits = text_splitter.split_documents(docs)
-
     vector_store = None
 
     try:
         vector_store = FAISS.load_local("faiss_vector_store", embeddings=embeddings, allow_dangerous_deserialization=True)
         print("Loaded FAISS index from local storage")
     except RuntimeError:
+        # s3://ragagentbucket/agent_data/test-mobile-mechanics.txt
+        loader = S3FileLoader(bucket="ragagentbucket", 
+                              key="agent_data/test-mobile-mechanics.txt", 
+                              aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), 
+                              aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"), 
+                              region_name=os.getenv("AWS_DEFAULT_REGION"))
+
+        docs = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        all_splits = text_splitter.split_documents(docs)
+
         vector_store = FAISS.from_documents(all_splits, embeddings)
         vector_store.save_local("faiss_vector_store")
         print("Saved FAISS index to local storage")
